264d6a70430c65f485e2d6428bdfd218cec55b4b
lhfu <fuluheng@gmail.com>
2016-04-07 13:47:34 +0800

    Update etc.py

--- etc.py
+++ etc.py
@@ -4,10 +4,11 @@ __author__ = 'f0x11'
 
 machine_no_dict = {
     'ubuntu': 1,
+    'li1460-85': 2,
 }
 
 mysql_user = 'root'
 mysql_passwd = '111111'
-mysql_host = '192.168.1.124'
+mysql_host = '172.17.0.2'
 mysql_port = 3306
 mysql_dbname = 'weixin'
